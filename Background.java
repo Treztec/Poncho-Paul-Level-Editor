2fb200ff69825491a3f1eb109d2e333541ce91f1
William Trent <gtrent9@gmail.com>
2014-10-08 12:53:02 -0500

    Acomplished Nothing

---
+++ Background.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Background here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Background extends World
+{
+
+    /**
+     * Constructor for objects of class Background.
+     * 
+     */
+    public Background()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1100, 600, 1, false);
+    }
+    
+    public 
+    
+}
---
+++ Element.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SaveTheWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class Element extends Actor
+{
+    public String toString() 
+    {
+        
